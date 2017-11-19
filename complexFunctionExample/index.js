dd48a998030321fcc8c6044927f81e1da04a7859
BenediktKrueger <benekrue@de.ibm.com>
2017-11-19 01:32:54 +0100

    environment Key

--- complexFunctionExample/index.js
+++ complexFunctionExample/index.js
@@ -1,9 +1,20 @@
 const leftPad = require("left-pad")
 
+var watson = require('watson-developer-cloud');
+
+
 function myAction(args) {
+
+    var conversation = watson.conversation({
+        username: args.conversationUsername,
+        password: args.conversationPassword,
+        version: 'v1',
+        version_date: args.version_date
+    });
+
     const lines = args.lines || [];
     //return { (padded: lines.map(l => leftPad(l, 30, "."))) + msg.conversationKey }
-    return { padded: args.conversationKey }
+    return { padded: args.conversationUsername }
 
 }
 
