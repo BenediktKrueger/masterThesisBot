7031b6e11c818a3b64345cd7299f5eb9f422b5d8
BenediktKrueger <benekrue@de.ibm.com>
2017-11-19 01:19:46 +0100

    environment Key

--- complexFunctionExample/index.js
+++ complexFunctionExample/index.js
@@ -3,7 +3,7 @@ const leftPad = require("left-pad")
 function myAction(args) {
     const lines = args.lines || [];
     //return { (padded: lines.map(l => leftPad(l, 30, "."))) + msg.conversationKey }
-    return { args.conversationKey }
+    return { padded: args.conversationKey }
 
 }
 
